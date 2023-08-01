0834dee6af812b492cbcaab9c8549cb6e08a409e
Shubham Dubey <95680495+iamskedy@users.noreply.github.com>
2023-08-01 16:35:24 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "shubhamkumar.dubey02@gmail.com";
+$sitename = "Shuhbam Kumar Dubey";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
